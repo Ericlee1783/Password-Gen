df4195235ac068ff5f37a6958b07e267e3b7932a
Ericlee1783 <ericlee1783@gmail.com>
2023-09-10 22:31:26 -0700

    updating java

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,12 @@
 // Assignment Code
+var lowerCaseLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+console.log(lowerCaseLetters);
+
+var UpperCaseLetters = lowerCaseLetters.toUpperCase();
+console.log(UpperCaseLetters);
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,3 +20,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
