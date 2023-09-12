181fd31844c71ca8841a26624521d21c3ce8c049
Ericlee1783 <ericlee1783@gmail.com>
2023-09-11 21:55:07 -0700

    savivng point

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,36 @@
 // Assignment Code
-var lowerCaseLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-console.log(lowerCaseLetters);
+var LowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-var UpperCaseLetters = lowerCaseLetters.toUpperCase();
-console.log(UpperCaseLetters);
+var UpperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
+var numbers = [0,1,2,3,4,5,6,7,8,9];
 
+var specialChar = ['!', '@' ,'#' ,'$' ,'%' ,'^' ,'&' ,'*' ,'(' ,')' ,'_' ,'+' ,'[' ,']' ,'{' ,'}' ,'|' ,';', ',' ,':' ,'.' ,'<' ,'>' ,'?' ,'"'];
 
-var generateBtn = document.querySelector("#generate");
+var allChoices = LowerCaseLetters.concat(UpperCaseLetters, numbers, specialChar);
+console.log(allChoices);
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var result = "";
 
-  passwordText.value = password;
+var getPasswordOptions = function() {
+  var user = window.prompt("How long would you like your password to be?")
 
+    var index = Math.floor(Math.random() * allChoices.length);
+    var password = allChoices[index];
 }
+getPasswordOptions()
+
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+// writePassword()
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
+// generateBtn.addEventListener("click", writePassword)
